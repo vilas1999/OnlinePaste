c2fd1807079c5022c7f35cab59d3d2ab878f31a3
Vilas M <vilas@Vilass-MacBook-Air.local>
2024-09-30 21:06:11 +0530

    Sprint boot project setup

---
+++ src/main/java/com/onlinepaste/onlinePaste/OnlinePasteApplication.java
@@ -0,0 +1,12 @@
+package com.onlinepaste.onlinePaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePasteApplication.class, args);
+	}
+}
